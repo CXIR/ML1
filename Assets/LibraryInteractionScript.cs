c751b93cda5954740b222a1b6ec93a6d7c955788
CXIR <cxir.developer@gmail.com>
2019-03-27 16:09:50 +0100

    Initial Commit

---
+++ Assets/LibraryInteractionScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraryInteractionScript : MonoBehaviour
+{
+    public Transform[] _plan;
+    public Transform[] _red;
+
+    public IntPtr _w;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _w = new IntPtr();
+
+        List<double> inputs = new List<double>();
+        List<double> outputs = new List<double>();
+
+        foreach (Transform sphere in _red)
+        {
+            inputs.Add(sphere.position.x);
+            inputs.Add(sphere.position.z);
+            outputs.Add(sphere.position.y);
+        }
+
+       _w = LibraryWraper.Initialize(2);
+
+        double[] i = inputs.ToArray();
+        double[] o = inputs.ToArray();
+
+        LibraryWraper.Train(_w, i, 2, _red.Length, o, 0.001, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform sphere in _plan)
+        {
+            List<double> localPositions = new List<double>()
+            {
+                sphere.position.x,
+                sphere.position.z
+            };
+            int predict = LibraryWraper.Predict(_w, localPositions.ToArray(), 2);
+
+            Vector3 item = new Vector3(sphere.position.x, sphere.position.y, sphere.position.z)
+            {
+                y = (predict == -1) ? -1 : 1
+            };
+
+            sphere.position = item;
+        }
+    }
+}
---
+++ Assets/LibraryWraper.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+public static class LibraryWraper
+{
+    [DllImport("libtest")]
+    public static extern int Return42(int valuesByModel);
+
+    [DllImport("libtest")]
+    public static extern IntPtr Initialize(int valuesByModel);
+
+    [DllImport("libtest")]
+    public static extern void Train(IntPtr w, double[] inputs, int valuesUsedByModel, int modelsTested, double[] valuesExpected, double learningStep, int iteration);
+
+    [DllImport("libtest")]
+    public static extern int Predict(IntPtr w, double[] inputs, int valuesUsedByModel);
+}
